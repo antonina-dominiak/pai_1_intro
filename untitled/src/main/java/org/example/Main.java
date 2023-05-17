620708efa48b8fbd8642de7e9fb2fa1afec90927
antonina-dominiak <antonina.dominiak01@gmail.com>
2023-05-17 16:41:43 +0200

    commit z poczatkiem kodu

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -6,5 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        String adresUrl = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+        // wyslanie requestu
+        String response = request();
+
+        // obrobka odpowiedzi
+        process(response);
+    }
+
+    private static String request() {
+        return "";
+
+    }
+    private static void process(String response1) {
+        System.out.println(response1);
     }
 }
