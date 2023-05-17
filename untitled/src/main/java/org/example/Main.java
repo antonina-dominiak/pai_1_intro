fafac527a89836cfc0b391ab8f6c508a8f94009e
antonina-dominiak <antonina.dominiak01@gmail.com>
2023-05-17 17:16:24 +0200

    client1

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+import static java.net.http.HttpResponse.BodyHandlers.ofString;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -7,17 +14,30 @@ public class Main {
     public static void main(String[] args) {
 
 
-        String adresUrl = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+        String url = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
         // wyslanie requestu
-        String response = request();
+        String response = request(url);
 
         // obrobka odpowiedzi
         process(response);
     }
 
-    private static String request() {
-        return "";
-
+    private static String request(String url1) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(url1))
+                    .GET()
+                    .build();
+            HttpClient client = HttpClient.newHttpClient();
+            var response = client.send(request, ofString());
+            if (response.statusCode() != 200) {
+                System.exit(1);
+                //status to kod erroru
+            }
+            return response.body();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
     private static void process(String response1) {
         System.out.println(response1);
