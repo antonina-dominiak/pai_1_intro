2b5b2956ad226671995a11f51d8715c87cff9550
antonina-dominiak <antonina.dominiak01@gmail.com>
2023-05-18 21:32:07 +0200

    Klient 3, backoffs and retries

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,19 @@
 package org.example;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpTimeoutException;
 import java.time.Duration;
-
-import static java.net.http.HttpResponse.BodyHandlers.ofString;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
-    public static void main(String[] args) {
+    private static final int N = 3;
 
+    public static void main(String[] args) {
 
         String url = "https://httpbin.org/delay/2";
         // wyslanie requestu
@@ -21,24 +21,44 @@ public class Main {
 
         // obrobka odpowiedzi
         process(response);
+
     }
 
     private static String request(String url1) {
         try {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI(url1))
-                    .timeout(Duration.ofSeconds(100))
-                    //.GET()
-                    .POST(HttpRequest.BodyPublishers.ofString("{\"imie\":\"natalia\"}"))
-                    .header("Content-Type", "application/json")
-                    .build();
             HttpClient client = HttpClient.newHttpClient();
-            var response = client.send(request, ofString());
+
+            int retryCount = 0;
+            int backoffDelay = 1;
+            while (retryCount < N) {
+                try {
+                    HttpRequest request = HttpRequest.newBuilder()
+                            .uri(new URI(url1))
+                            .timeout(Duration.ofSeconds(backoffDelay))
+                            //.GET()
+                            .POST(HttpRequest.BodyPublishers.ofString("{\"imie\":\"natalia\"}"))
+                            .header("Content-Type", "application/json")
+                            .build();
+                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                    if (response.statusCode() == 200) {
+                        return response.body();
+                    }
+
+                } catch (HttpTimeoutException e) {
+
+                }
+                retryCount++;
+                Thread.sleep(1000 * backoffDelay);
+                backoffDelay ^= 2;
+            }
+
+            return null;
+
+/*            var response = client.send(request, ofString());
             if (response.statusCode() != 200) {
                 System.exit(1);
-                //status to kod erroru
             }
-            return response.body();
+            return response.body();*/
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
